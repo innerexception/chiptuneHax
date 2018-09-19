d1b700de2653e67de1451ab41151e3fbb343a208
jbaker <jbakerdev@gmail.com>
2018-09-19 13:31:16 -0400

    flood almost working

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -3,71 +3,63 @@ import Phaser from 'phaser'
 import {icons, gridWidth, gridHeight} from '../config'
 
 export default class extends Phaser.Scene {
-  constructor () {
-    super({ key: 'GameScene' })
-  }
-  init () {}
-  preload () {}
+    constructor () {
+        super({ key: 'GameScene' })
+    }
+    init () {}
+    preload () {}
   
-  create () {
+    create () {
 
-    this.background = this.add.image(400,0,'background')
-    this.background.scaleX=2
-    this.background.scaleY=2
+        this.background = this.add.image(400,0,'background')
+        this.background.scaleX=2
+        this.background.scaleY=2
 
-    this.icons = icons.map((icon)=>this.createIcon(icon))
+        this.icons = icons.map((icon)=>this.createIcon(icon))
 
-    this.grid = new Array(gridWidth).fill().map((row, x) => {
-      return new Array(gridHeight).fill().map((col, y) => {
-        var sx = 140 + (x * 38)
-        var sy = 50 + (y * 38) //we use this y as the final position for the animation later
-        let asset = getRandomAssetName()
-        let image = this.add.image(sx, 0, 'sprites', asset+'Inactive')
-        image.setData('y', sy);
-        image.setData('asset', asset)
-        return image
-      })
-    })
-    this.matched = []
+        this.grid = new Array(gridWidth).fill().map((row, x) => {
+            return new Array(gridHeight).fill().map((col, y) => {
+            var sx = 140 + (x * 38)
+            var sy = 50 + (y * 38) //we use this y as the final position for the animation later
+            let asset = getRandomAssetName()
+            let image = this.add.image(sx, 0, 'sprites', asset+'Inactive')
+            image.setData('y', sy);
+            image.setData('asset', asset)
+            return image
+            })
+        })
+        this.matched = []
 
-    this.seedGrid()
+        this.seedGrid()
 
-    this.matched.forEach((match) => {
-      let asset = match.getData('newAsset')+'Active';
-      match.setFrame(asset);
-      match.setData('asset', asset)
-    })
+        this.playerColor = this.grid[0][0].getData('asset');
+        this.particles = this.add.particles('sprites');
+        this.emitters = {}
+        icons.map((icon) => {
+            this.createEmitter(icon.asset)
+        })
 
-    this.playerColor = this.grid[0][0].getData('asset');
-    this.particles = this.add.particles('sprites');
-    this.emitters = {}
-    icons.map((icon) => {
-      this.createEmitter(icon.asset)
-    })
+        this.moves = 20
+        this.revealGrid()
+        this.allowClick = true
 
-    
+        // this.mushroom = new Mushroom({
+        //   scene: this,
+        //   x: 400,
+        //   y: 300,
+        //   asset: 'mushroom'
+        // })
 
-    this.moves = 20
-    this.revealGrid()
-    this.allowClick = true
+        // this.add.existing(this.mushroom)
+        // this.add.text(100, 100, 'Phaser 3 - ES6 - Webpack ', {
+        //   font: '64px Bangers',
+        //   fill: '#7744ff'
+        // })
+    }
 
-    // this.mushroom = new Mushroom({
-    //   scene: this,
-    //   x: 400,
-    //   y: 300,
-    //   asset: 'mushroom'
-    // })
-  
-    // this.add.existing(this.mushroom)
-    // this.add.text(100, 100, 'Phaser 3 - ES6 - Webpack ', {
-    //   font: '64px Bangers',
-    //   fill: '#7744ff'
-    // })
-  }
-
-  revealGrid = () =>
-  {
-      this.tweens.add({
+    revealGrid = () =>
+    {
+        this.tweens.add({
         targets: this.grid[0][0],
         scaleX: '+=0.2',
         scaleY: '+=0.2',
@@ -75,98 +67,107 @@ export default class extends Phaser.Scene {
         duration: 900,
         yoyo: true,
         repeat: -1
-      });
+        });
 
-      this.tweens.add({
-          targets: this.background,
-          y: 300,
-          ease: 'Power3'
-      });
+        this.tweens.add({
+            targets: this.background,
+            y: 300,
+            ease: 'Power3'
+        });
 
-      var i = 800;
+        var i = 800;
 
-      for (var y = 13; y >= 0; y--)
-      {
-          for (var x = 0; x < 14; x++)
-          {
-              var block = this.grid[x][y];
+        for (var y = 13; y >= 0; y--)
+        {
+            for (var x = 0; x < 14; x++)
+            {
+                var block = this.grid[x][y];
 
-              this.tweens.add({
+                this.tweens.add({
 
-                  targets: block,
+                    targets: block,
 
-                  y: block.getData('y'),
+                    y: block.getData('y'),
 
-                  ease: 'Power3',
-                  duration: 800,
-                  delay: i
+                    ease: 'Power3',
+                    duration: 800,
+                    delay: i
 
-              });
+                });
 
-              i += 20;
-          }
-      }
+                i += 20;
+            }
+        }
 
-      i -= 1000;
+        i -= 1000;
 
-      //  Icons
-      this.icons.forEach((icon) => {
+        //  Icons
+        this.icons.forEach((icon) => {
         this.tweens.add({
             targets: [ icon ],
             x: icon.x,
             ease: 'Power3',
             delay: i
         });
-      })
-      
-      //  Text
+        })
+        
+        //  Text
 
-      // this.tweens.add({
-      //     targets: [ this.text1, this.text2 ],
-      //     alpha: 1,
-      //     ease: 'Power3',
-      //     delay: i
-      // });
+        // this.tweens.add({
+        //     targets: [ this.text1, this.text2 ],
+        //     alpha: 1,
+        //     ease: 'Power3',
+        //     delay: i
+        // });
 
-      // i += 500;
+        // i += 500;
 
-      // var movesTween = this.tweens.addCounter({
-      //     from: 0,
-      //     to: 25,
-      //     ease: 'Power1',
-      //     onUpdate: function (tween, targets, text)
-      //     {
-      //         text.setText(Phaser.Utils.String.Pad(tween.getValue().toFixed(), 2, '0', 1));
-      //     },
-      //     onUpdateParams: [ this.text2 ],
-      //     delay: i
-      // });
+        // var movesTween = this.tweens.addCounter({
+        //     from: 0,
+        //     to: 25,
+        //     ease: 'Power1',
+        //     onUpdate: function (tween, targets, text)
+        //     {
+        //         text.setText(Phaser.Utils.String.Pad(tween.getValue().toFixed(), 2, '0', 1));
+        //     },
+        //     onUpdateParams: [ this.text2 ],
+        //     delay: i
+        // });
 
-      // i += 500;
+        // i += 500;
 
-      this.tweens.add({
-          targets: [ this.arrow ],
-          alpha: 1,
-          ease: 'Power3',
-          delay: i
-      });
+        this.tweens.add({
+            targets: [ this.arrow ],
+            alpha: 1,
+            ease: 'Power3',
+            delay: i
+        });
 
-      this.time.delayedCall(i, this.startInputEvents, [], this);
-  }
+        this.grid[0][0].setFrame(this.grid[0][0].getData('asset')+'Active')
 
-  startInputEvents= () => 
-  {
-      // this.input.on('gameobjectover', this.onIconOver, this);
-      // this.input.on('gameobjectout', this.onIconOut, this);
-      this.input.on('gameobjectdown', this.onIconDown, this);
+        this.time.delayedCall(i, this.startInputEvents, [], this);
+    }
 
-      // this.input.keyboard.on('keydown_X', function () {
+    startInputEvents= () => 
+    {
+        // this.input.on('gameobjectover', this.onIconOver, this);
+        // this.input.on('gameobjectout', this.onIconOut, this);
+        this.input.on('gameobjectdown', this.onIconDown, this);
 
-      //     this.moves--;
-      //     this.text2.setText(Phaser.Utils.String.Pad(this.moves, 2, '0', 1));
+        // this.input.keyboard.on('keydown_X', function () {
 
-      // }, this);
-  }
+        //     this.moves--;
+        //     this.text2.setText(Phaser.Utils.String.Pad(this.moves, 2, '0', 1));
+
+        // }, this);
+    }
+
+    stopInputEvents= () =>
+    {
+        // this.input.off('gameobjectover', this.onIconOver);
+        // this.input.off('gameobjectout', this.onIconOut);
+        this.input.off('gameobjectdown', this.onIconDown);
+    }
 
     onIconDown = (pointer, gameObject) =>
     {
@@ -177,25 +178,25 @@ export default class extends Phaser.Scene {
 
         var icon = gameObject;
 
-        var asset = icon.getData('asset');
+        var clickedAsset = icon.getData('asset');
 
-        //  Valid color?
-        if (asset === this.currentColor)
+        //  Clicked color must not match the current 0,0 color
+        if (clickedAsset === this.currentColor)
         {
             return;
         }
 
-        var oldAsset = this.grid[0][0].getData('asset');
+        var originAsset = this.grid[0][0].getData('asset');
 
-        if (oldAsset !== asset)
+        if (originAsset !== clickedAsset)
         {
-            this.currentColor = asset;
+            this.currentColor = clickedAsset;
 
             this.matched = [];
 
             this.moves--;
 
-            this.performHack(oldAsset, asset, 0, 0);
+            this.performHack(originAsset, clickedAsset, 0, 0);
 
             if (this.matched.length > 0)
             {
@@ -204,71 +205,80 @@ export default class extends Phaser.Scene {
         }
     }
 
-  createEmitter= (asset) =>
-  {
-      this.emitters[asset] = this.particles.createEmitter({
-          frame: asset,
-          lifespan: 1000,
-          speed: { min: 300, max: 400 },
-          alpha: { start: 1, end: 0 },
-          scale: { start: 0.5, end: 0 },
-          rotate: { start: 0, end: 360, ease: 'Power2' },
-          blendMode: 'ADD',
-          on: false
-      });
-  }
-
-  seedGrid = () => {
-    for (var i = 0; i < 8; i++)
+    createEmitter= (asset) =>
     {
-        var x = Phaser.Math.Between(0, 13)
-        var y = Phaser.Math.Between(0, 13)
-
-        var oldAsset = this.grid[x][y].getData('asset')
-        var newAsset = getRandomAssetName()
-
-        this.performHack(oldAsset, newAsset, x, y)
+        this.emitters[asset] = this.particles.createEmitter({
+            frame: asset+'Active',
+            lifespan: 1000,
+            speed: { min: 300, max: 400 },
+            alpha: { start: 1, end: 0 },
+            scale: { start: 0.5, end: 0 },
+            rotate: { start: 0, end: 360, ease: 'Power2' },
+            blendMode: 'ADD',
+            on: false
+        });
     }
-  }
 
-  performHack = (oldAsset, newAsset, x, y) =>
+    seedGrid = () => {
+        for (var i = 0; i < 8; i++)
+        {
+            var x = Phaser.Math.Between(0, 13)
+            var y = Phaser.Math.Between(0, 13)
+            this.performHack(this.grid[x][y].getData('asset'), getRandomAssetName(), x, y)
+        }
+        this.matched.forEach((match) => {
+            match.setFrame(match.getData('asset')+'Inactive')
+        })
+    }
+
+    performHack = (originAsset, targetAsset, x, y) =>
     {
-        if (oldAsset === newAsset || this.grid[x][y].getData('asset') !== oldAsset)
+        const block = this.grid[x][y]
+        const currentBlockAsset = block.getData('asset');
+
+        if(currentBlockAsset !== originAsset || originAsset === targetAsset){
+            if(originAsset === targetAsset){
+                if (this.matched.indexOf(block) === -1)
+                {
+                    this.matched.push(block);
+                }
+            }
+            return
+        } 
+
+        if (this.matched.indexOf(block) === -1)
         {
-            return;
+            this.matched.push(block);
         }
 
-        this.grid[x][y].setData('newAsset', newAsset)
-
-        if (this.matched.indexOf(this.grid[x][y]) === -1)
-        {
-            this.matched.push(this.grid[x][y])
-        }
-        else return;
-
+        //Want to make all blocks that match the origin texture into the target texture
+        block.setData('asset', targetAsset)
         if (x > 0)
         {
-            this.performHack(oldAsset, newAsset, x - 1, y);
+            this.performHack(originAsset, targetAsset, x - 1, y);
         }
 
         if (x < 13)
         {
-            this.performHack(oldAsset, newAsset, x + 1, y);
+            this.performHack(originAsset, targetAsset, x + 1, y);
         }
 
         if (y > 0)
         {
-            this.performHack(oldAsset, newAsset, x, y - 1);
+            this.performHack(originAsset, targetAsset, x, y - 1);
         }
 
         if (y < 13)
         {
-            this.performHack(oldAsset, newAsset, x, y + 1);
+            this.performHack(originAsset, targetAsset, x, y + 1);
         }
+        return
+        
     }
 
     resolveHack = () =>
     {
+        //Want to fire emitters in a nice order
         this.matched.sort((a, b) => {
 
             var aDistance = Phaser.Math.Distance.Between(a.x, a.y, 166, 66);
@@ -286,18 +296,17 @@ export default class extends Phaser.Scene {
         this.allowClick = false;
 
         this.matched.forEach((block) => {
-          var newAsset = block.getData('newAsset');
           var asset = block.getData('asset');
 
           var emitter = this.emitters[asset];
 
-          this.time.delayedCall(t, function (block, newAsset) {
+          this.time.delayedCall(t, (block, newAsset, emitter) => {
 
-              block.setFrame(newAsset);
+              block.setFrame(newAsset+'Active');
 
               emitter.explode(6, block.x, block.y);
               
-          }, [ block, newAsset, emitter ]);
+          }, [ block, asset, emitter ]);
 
           t += inc;
         })
@@ -334,6 +343,29 @@ export default class extends Phaser.Scene {
         }
 
         return true;
+    }
+    gameWon= () =>
+    {
+        this.stopInputEvents();
+        this.time.delayedCall(2000, this.boom, [], this);
+    }
+    gameLost= () =>
+    {
+        this.stopInputEvents();
+        this.time.delayedCall(2000, this.boom, [], this);
+    }
+
+    boom=() =>
+    {
+        var color = Phaser.Math.RND.pick(icons).asset;
+
+        this.emitters[color].explode(8, Phaser.Math.Between(128, 672), Phaser.Math.Between(28, 572))
+
+        color = Phaser.Math.RND.pick(icons).asset;
+
+        this.emitters[color].explode(8, Phaser.Math.Between(128, 672), Phaser.Math.Between(28, 572))
+
+        this.time.delayedCall(100, this.boom, [], this);
     }
 
   createIcon = ({asset, x, y}) => this.add.image(x, y, 'sprites', asset+'Active').setOrigin(0).setInteractive().setData('asset', asset);
